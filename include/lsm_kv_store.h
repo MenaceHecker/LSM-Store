8475350a4e31eb560c8cab71339d71e14544d647
Tushar Mishra <thedynamic2508@gmail.com>
2026-05-29 15:24:27 -0400

    Added override requirement

--- include/lsm_kv_store.h
+++ include/lsm_kv_store.h
@@ -6,7 +6,7 @@
 class LSMKVStore final : public KVStore {
 public:
     void put(const std::string& key, const std::string& value) override;
-    lsm::optional<std::string> get(const std::string& key) override;
+    lsm::optional<std::string> get(const std::string& key) const override;
     void del(const std::string& key) override;
 
     size_t memtable_size() const;
