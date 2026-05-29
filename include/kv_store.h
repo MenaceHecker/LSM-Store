feb7a2a2763d6a944eb22fe1af5622e6fe871cc6
Tushar Mishra <thedynamic2508@gmail.com>
2026-05-29 15:23:35 -0400

    Updated value of constant to 0

--- include/kv_store.h
+++ include/kv_store.h
@@ -5,7 +5,7 @@
 class KVStore {
 public:
     virtual void put(const std::string& key, const std::string& value) = 0;
-    virtual lsm::optional<std::string> get(const std::string& key) = 0;
+    virtual lsm::optional<std::string> get(const std::string& key) const = 0;
     virtual void del(const std::string& key) = 0;
     virtual ~KVStore() = default;
 };
